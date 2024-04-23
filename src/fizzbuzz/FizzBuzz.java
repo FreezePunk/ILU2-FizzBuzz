90ca83e8f4b4204b5098728c3958ff2814e7d381
Mohamed <83366998+FreezePunk@users.noreply.github.com>
2024-04-23 16:41:13 +0200

    Update FizzBuzz.java

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -3,6 +3,19 @@ package fizzbuzz;
 public class FizzBuzz {
 	
 	public static Object fizzbuzz(int i) {
-		return "1";
+		String res="";
+		if (i%3==0)
+		{
+			res=res+"fizz";
+		}
+		if (i%5==0)
+		{
+			res=res+"buzz";
+		}
+		if (res.equals("")) {
+			return Integer.toString(i);
+		}
+		else
+			return res;
 	}
 }
