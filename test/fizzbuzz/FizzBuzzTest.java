d3bf939c78f938298bb4e559fbc7d2b7a03c4db4
Mohamed <83366998+FreezePunk@users.noreply.github.com>
2024-04-23 16:40:51 +0200

    Update FizzBuzzTest.java

--- test/fizzbuzz/FizzBuzzTest.java
+++ test/fizzbuzz/FizzBuzzTest.java
@@ -15,5 +15,39 @@ class FizzBuzzTest {
 	void Niveau1() {
 		assertEquals(FizzBuzz.fizzbuzz(1),"1");
 	}
+	
+	@Test
+	void Niveau2() {
+		assertEquals(FizzBuzz.fizzbuzz(2),"2");
+	}
+	
+	@Test
+	void Niveau3() {
+		assertEquals(FizzBuzz.fizzbuzz(3),"fizz");
+	}
+	
+	@Test
+	void Niveau4() {
+		assertEquals(FizzBuzz.fizzbuzz(4),"4");
+		assertEquals(FizzBuzz.fizzbuzz(7),"7");
+	}
+	
+	@Test
+	void Niveau5() {
+		assertEquals(FizzBuzz.fizzbuzz(6),"fizz");
+	}
 
+	@Test
+	void Niveau6() {
+		assertEquals(FizzBuzz.fizzbuzz(5),"buzz");
+		assertEquals(FizzBuzz.fizzbuzz(10),"buzz");
+		assertEquals(FizzBuzz.fizzbuzz(500),"buzz");
+	}
+	
+	@Test
+	void Niveau7() {
+		assertEquals(FizzBuzz.fizzbuzz(15),"fizzbuzz");
+		assertEquals(FizzBuzz.fizzbuzz(2*5*3),"fizzbuzz");
+		assertEquals(FizzBuzz.fizzbuzz(2*5*3*3),"fizzbuzz");
+	}
 }
